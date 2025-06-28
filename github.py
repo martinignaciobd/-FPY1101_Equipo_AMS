004ffa8e22474d6224ea533d97f642847e538650
Martin Bustamante <mar.bustamanted@duocuc.cl>
2025-06-28 12:30:11 -0400

    Add files via upload

---
+++ github.py
@@ -0,0 +1,20 @@
+# Menú base del programa
+while True:
+    print("\n--- MENÚ PRINCIPAL ---")
+    print("1. Función de integrante 1")
+    print("2. Función de integrante 2")
+    print("3. Función de integrante 3")
+    print("0. Salir ")
+
+    op = input("Selecciona una opcion : ")
+    if op == "0":
+        print("Programa finalizado.")
+        break
+    elif op == "1":
+        pass #funcion integrante 1
+    elif op == "2":
+        pass #funcion integrante 2
+    elif op == "3":
+        pass #funcion integrante 3
+    else:
+        print("Opcion invalida.")
